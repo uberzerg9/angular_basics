942884c50465b8c521c05baf8a1d39e171b19270
Alexander Stavar <uberzerg9@gmail.com>
2022-12-10 22:40:18 +0200

    add interpolation and data-binding examples

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.css']
 })
-export class FirstComponent {
+export class FirstComponent implements OnInit {
+  propertyOne = 'Hello';
+  propertyTwo = 111;
+  propertyBool = true;
+  propertyObj1 = {};
+  propertyObj2 = {
+    prop1: 'some value'
+  };
+  emptyProp:any;
+  notEmptyProp;
 
+  constructor() {
+    this.notEmptyProp = 'not empty';
+    this.startInterval();
+  }
+
+  returnStringMethod() {
+    return 'hello some string text';
+  }
+
+  counter: number = 0;
+  startInterval() {
+    setInterval(() => {
+      this.counter++;
+    }, 1000)
+  }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/second/second.component.ts
+++ src/app/second/second.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-second',
   templateUrl: './second.component.html',
   styleUrls: ['./second.component.css']
 })
-export class SecondComponent {
+export class SecondComponent implements OnInit {
+  firstImageUrl:string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/1200px-Angular_full_color_logo.svg.png';
+  secondImageUrl:string = 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/800px-Unofficial_JavaScript_logo_2.svg.png';
 
+  firstImageWidth:number = 300;
+  secondImageWidth:number = 250;
+
+  flag = true;
+
+  constructor() {}
+  ngOnInit() {}
 }
