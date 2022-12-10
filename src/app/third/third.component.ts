e3e8a00b3e48b381cc1549ce12fc970ebe13489f
Alexander Stavar <uberzerg9@gmail.com>
2022-12-10 22:56:59 +0200

    event binding

--- src/app/third/third.component.ts
+++ src/app/third/third.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-third',
   templateUrl: './third.component.html',
   styleUrls: ['./third.component.css']
 })
-export class ThirdComponent {
+export class ThirdComponent implements OnInit {
 
+  counter:number = 0;
+
+  increment() {
+    this.counter++;
+  }
+
+  decrement() {
+    this.counter--;
+  }
+
+  consoleCounter(e:any) {
+    alert(e);
+  }
+
+  consoleEventObj1(e:any) {
+    alert(e);
+    alert(e.type);
+  }
+
+  consoleEventObjValue(e:any) {
+    alert(e);
+  }
+
+  a = 'old';
+  b = ' text1';
+
+  constructor() {}
+  ngOnInit() {}
 }
