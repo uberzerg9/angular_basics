f5cf2f97aa46895bdad39040ea03013b7ad7c79d
Alexander Stavar <uberzerg9@gmail.com>
2022-12-10 21:45:04 +0200

    add routing between components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
+import { FourthComponent } from './fourth/fourth.component';
+
+const routes: Routes = [
+  {path: 'first', component: FirstComponent},
+  {path: 'second', component: SecondComponent},
+  {path: 'third', component: ThirdComponent},
+  {path: 'fourth', component: FourthComponent},
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    ThirdComponent,
+    FourthComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
